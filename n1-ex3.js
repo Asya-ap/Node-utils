b5429b7d755ca6c6d0bb0ffa92f342c27d38bf7b
Asya <a.potapovafilina@gmail.com>
2021-03-23 10:48:56 +0100

    correcting exercise n1-ex3

--- n1-ex3.js
+++ n1-ex3.js
@@ -1,13 +1,22 @@
-const http = require('http');
+// const http = require('http');
 
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    res.setHeader('Content-type', 'text/plain');
-    res.write('Love you to the moon and back');
-    res.end();
-});
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method);
+//     res.setHeader('Content-type', 'text/plain');
+//     res.write('Love you to the moon and back');
+//     res.end();
+// });
 
-server.listen(3000, 'localhost', () => {
-    console.log('listening for request on port 3000');
+// server.listen(3000, 'localhost', () => {
+//     console.log('listening for request on port 3000');
+// });
+
+const fs = require('fs');
+
+fs.readFile('./newFile.txt', (err, data) =>{
+    if(err){
+        console.log(err);
+    }
+    console.log(data.toString());
 });
